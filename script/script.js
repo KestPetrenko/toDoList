40f590b96763cfb76377d5ec1f044d7c543705d6
Konstantin Petrenko <109843396+KestPetrenko@users.noreply.github.com>
2022-09-16 20:07:25 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,100 +1,109 @@
-let taskNameInput = document.querySelector("#task-name-input");
-    let addTaskButton = document.querySelector("#add-task-btn");
-    let startMessage = document.querySelector("#start-message");
-    let taskList = document.querySelector(".task-list");
-    
+let div1 = document.querySelector("#generateButton");
+let div2 = document.querySelector("#interactivePanel");
+let div3 = document.querySelector("#top");
 
-    addTaskButton.addEventListener("click", addTaskHandler);
-    function createTask(text) {
-        let div = document.createElement("div");
-        div.classList.add("task");
+div1.onclick = generateElementArea;
 
-        let input = document.createElement("input");
-        input.classList.add("one");
-        input.addEventListener("click", changeTaskState); 
-        input.type = "checkbox";
+function generateElementArea() {
+    var text = document.createElement("textarea");
+    div2.append(text);
+
+    document.querySelector("textarea").placeholder = 'Введіть текст і натисніть Enter';
+
+    text.addEventListener("keydown", function (e) {
+        if (e.code == 'Enter') generateElement();
+        text.addEventListener("keyup", function (e) {
+            if (e.code == 'Enter')
+                text.remove()
+        })
+
+        console.log(text.value)
 
 
-        let p = document.createElement("p");
-        p.innerText = text;
+        function generateElement() {
+            let div = document.createElement("div");
+            div.classList.add("interactiveElement");
+            let maxLeft = window.innerWidth - 50;
+            let maxTop = window.innerHeight - 150;
+            div.style.top = getRandomValue(maxTop) + "px";
+            div.style.left = getRandomValue(maxLeft) + "px";
+            div.style.backgroundColor = getRandomColor();
+            div.style.backgroundColor = getRandomColor();
+            div.style.rotate = getRandomRotate();
 
-        let button = document.createElement("button");
-        button.classList.add("list__delete");
-        let divText = document.createTextNode("Видалити");
+            let button = document.createElement("button");
+            button.classList.add("delete");
+            let divText = document.createTextNode("X");
+            button.append(divText);
+            function getRandomRotate() {
+                var angle = (Math.floor(Math.random() * (35 - 15) - 15)) + "deg";
+                div.style.transform = "rotate(" + angle + ")";
+                div.addEventListener("mouseover", mouseOverHanderl);
+                div.addEventListener("mouseout", mouseOutHanderl);
+                function mouseOverHanderl() {
+                    div.style.width = "40vw";
+                    div.style.height = "40vw";
+                    div.style.border = "6px solid black";
+                }
+                function mouseOutHanderl() {
+                    div.style.width = "25vw";
+                    div.style.height = "25vw";
+                    div.style.border = "none";
+                }
+            };
 
-        button.append(divText);
-        div.append(input);
-        div.append(p);
-        div.append(button);
-
-        button.addEventListener("click", handler);
-        function handler() {
-            div.remove();
-        }
-
-        let Btn = document.querySelector("#myBtn1");
-        Btn.addEventListener("click", handler);
-        function handler() {
-            div.remove();
-        }
-
-        return div;
-    }
-
-    function changeTaskState() {
-        if (this.checked) { 
-            this.parentElement.classList.add("completed");
-        } else { 
-            this.parentElement.classList.remove("completed");
-        }
-    }
-
-  
-    function addTaskHandler() {
-        if (taskNameInput.value) { 
-            if (!startMessage.hidden) startMessage.hidden = true;
-
-            let newTask = createTask(taskNameInput.value);
-            taskList.append(newTask);
-            taskNameInput.value = "";
-        } else { 
-            alert("Введіть задачу");
-        }
-    }
-
-    var video = document.getElementById("myVideo");
-    var btn = document.getElementById("myBtn");
-    function myFunction() {
-        if (video.paused) {
-            video.play();
-            btn.innerHTML = "Pause";
-        } else {
-            video.pause();
-            btn.innerHTML = "Play";
-        }
-    }
-
-    let BtnSpecial = document.querySelector("#delBtnCpecial");
-    BtnSpecial.addEventListener("click", function (e) {
-        let messages = document.querySelectorAll(".completed");
-        for (let i of messages) {
-            i.remove()
-        }
-        });
-
-    let hide = document.querySelector("#hide");
-    hide.addEventListener("click", function (e) {
-            let messages = document.querySelectorAll(".completed");
-            for (let i of messages) {
-                i.style.display = 'none';
+            div.append(text.value);
+            div.append(button);
+            div2.append(div);
+            button.addEventListener("click", handler);
+            function handler() {
+                div.remove();
             }
-        });
-
-let show = document.querySelector("#show");
-show.addEventListener("click", function (e) {
-            let messages = document.querySelectorAll(".completed");
-            for (let i of messages) {
-                i.style.display = '';
+            let Btn = document.querySelector("#delete1");
+            Btn.addEventListener("click", handler);
+            function handler() {
+                div.remove();
             }
-        });
+            function addTaskHandler() {
+                div.append(text.value);
+                text.remove();
+            }
 
+            return div;
+        }
+    })
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[getRandomValue(16)];
+    }
+    return color;
+}
+
+function getRandomValue(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let move = false;
+let offsetX, offsetY;
+let currentDiv;
+
+div2.addEventListener("mousedown", function (e) {
+    move = true;
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+    currentDiv = e.target;
+});
+
+div2.addEventListener("mousemove", function (e) {
+    if (move) {
+        currentDiv.style.top = e.clientY - offsetY + "px";
+        currentDiv.style.left = e.clientX - offsetX + "px";
+    }
+});
+div2.addEventListener("mouseup", function (e) {
+    move = false;
+});
